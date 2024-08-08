c47ca6159eecbcb74622525562ed59953e1e0e43
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-08 23:20:45 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,4 +5,9 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 const dateTimePicker = document.getElementById('datetime-picker');
-const startBtn = document.querySelector('[data-start]');
+const startBtn = document.querySelector('button[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const mins = document.querySelector('[data-minutes]');
+const secs = document.querySelector('[data-seconds]');
+
