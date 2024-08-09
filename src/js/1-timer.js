4ef7ec89364e37c714466a691b0fd362deba5510
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-09 18:42:38 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -8,6 +8,31 @@ const dateTimePicker = document.getElementById('datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
 const days = document.querySelector('[data-days]');
 const hours = document.querySelector('[data-hours]');
-const mins = document.querySelector('[data-minutes]');
-const secs = document.querySelector('[data-seconds]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+let selectedDate = null;
+let intervalId = null;
+
+startBtn.disabled = true;
+
+flatpickr(dateTimePicker, {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        handleDateSelection(selectedDates[0]);
+    },
+});
+
+function handleDateSelection(date) {
+    if (date <= new Date()) {
+        iziToast.error({ title: 'Error', message: 'Please choose a date in the future' });
+        startBtn.disabled = true;
+    } else {
+        selectedDate = date;
+        startBtn.disabled = false;
+    }
+}
 
