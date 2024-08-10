6c2c2a411f23b2770dad1c537837695d294a4718
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-10 23:11:40 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,4 +4,18 @@ import "izitoast/dist/css/iziToast.min.css";
 const formEl = document.querySelector('.form');
 formEl.addEventListener('submit', event => {
     event.preventDefault();
+
+    const delay = Number(event.target.elements.delay.value);
+    const state = event.target.elements.state.value;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    })
+
 })
