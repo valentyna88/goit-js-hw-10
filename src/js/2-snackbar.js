d7f1565fd4bc69b87832c948ee76d385855378c9
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-10 22:43:23 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,7 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+})
